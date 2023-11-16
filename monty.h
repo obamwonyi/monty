49bd7036b22fe689a6a5fd7db46e3960b47ad915
netweaver <developer.oduwa@gmail.com>
2023-11-16 23:03:35 +0100

    fixed monty.h betty error

--- monty.h
+++ monty.h
@@ -9,8 +9,6 @@
 #include <ctype.h>
 #include <stdarg.h>
 
-stack_t *head = NULL;
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -22,9 +20,9 @@ stack_t *head = NULL;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -37,8 +35,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t *head;
